3fbef741516cfbd16752baeea791db40cf886003
Marie Lewis <mariea1022@gmail.com>
2023-02-12 11:27:06 -0800

    fixed path to config.js

--- utils/seed.js
+++ utils/seed.js
@@ -1,4 +1,4 @@
-const connection = require("../config/connection");
+const connection = require("../config/config");
 const { Thought, User, Reaction } = require("../models");
 const {
   getRandomUsername,
